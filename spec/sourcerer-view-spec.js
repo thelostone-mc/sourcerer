d640fca13ce480d2267e8c6b486b7b23eb8e8a5c
Aditya Anand <adianand@paypal.com>
2017-09-25 23:17:45 +0530

    core: removed unwanted packages

--- spec/sourcerer-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SourcererView from '../lib/sourcerer-view';
-
-describe('SourcererView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
